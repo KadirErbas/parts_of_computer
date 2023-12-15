8a77016b30e5127af7219930c6651d18f9285a74
Abdulkadir Erbas <kdrerbas72@gmail.com>
2023-12-15 17:17:46 +0300

    category modeline image ve aciklama eklendi

--- parts_of_computer_app/models.py
+++ parts_of_computer_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # kategori
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, null=True)
     
     def __str__(self):
         return self.name
