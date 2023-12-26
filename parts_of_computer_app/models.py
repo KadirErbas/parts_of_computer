1400615c6d1015bb87c8d60d255d93517edc497e
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2023-12-26 13:46:25 +0300

    new image

--- parts_of_computer_app/models.py
+++ parts_of_computer_app/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # kategori
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, null=True)
     
     def __str__(self):
         return self.name
+    
     
 # ürün
 class Product(models.Model):
